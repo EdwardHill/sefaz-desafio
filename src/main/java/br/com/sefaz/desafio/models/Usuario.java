06020decb59301c17bf62a9eb3103165cf0fa3d7
Carlo <Carlo@DESKTOP-R2252RU>
2019-11-07 16:27:59 -0300

    alteração na estratégia de greção de chave primaria nas etidades, Usuario  e Telefone

--- src/main/java/br/com/sefaz/desafio/models/Usuario.java
+++ src/main/java/br/com/sefaz/desafio/models/Usuario.java
@@ -7,12 +7,13 @@ import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.GenericGenerator;
 
 
 
@@ -22,7 +23,9 @@ public class Usuario implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	//@GeneratedValue(strategy = GenerationType.AUTO)  
+	@GeneratedValue(generator = "increment")
+	@GenericGenerator(name = "increment", strategy="increment")
 	private Long id;
 
 	@NotNull (message = "O campo não deve estar vazio!")
