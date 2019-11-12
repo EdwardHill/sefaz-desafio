dd29a17280d5860a650532a0e2e5aabe0b45a4a6
carlos eduardo <carlo5_3duardo@hotmail.com>
2019-11-12 15:13:19 -0300

    estrategia

--- src/main/java/br/com/sefaz/desafio/models/Telefone.java
+++ src/main/java/br/com/sefaz/desafio/models/Telefone.java
@@ -20,7 +20,7 @@ public class Telefone implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY,generator = "increment")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "increment")
 	@GenericGenerator(name = "increment", strategy = "increment")
 	 
 	private Long id;
