6d004fc2d8928c72898c2e6305e29feefb57b340
carlos eduardo <carlo5_3duardo@hotmail.com>
2019-11-13 11:20:27 -0300

    nada

--- src/main/java/br/com/sefaz/desafio/models/Telefone.java
+++ src/main/java/br/com/sefaz/desafio/models/Telefone.java
@@ -1,22 +1,23 @@
 package br.com.sefaz.desafio.models;
 
-
+import java.io.Serializable;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-
+import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.validator.constraints.Range;
 
 @Entity
-public class Telefone  {
+public class Telefone implements Serializable {
 
-	
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	//@GeneratedValue(strategy = GenerationType.SEQUENCE)
