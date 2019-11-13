ca9cd50f93a30f1e6741f2492194cce0ec14edcd
carlos eduardo <carlo5_3duardo@hotmail.com>
2019-11-13 11:11:31 -0300

    nada

--- src/main/java/br/com/sefaz/desafio/models/Telefone.java
+++ src/main/java/br/com/sefaz/desafio/models/Telefone.java
@@ -1,32 +1,31 @@
 package br.com.sefaz.desafio.models;
 
-import java.io.Serializable;
+
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import org.hibernate.annotations.GenericGenerator;
+
 import org.hibernate.validator.constraints.Range;
 
 @Entity
-public class Telefone implements Serializable {
+public class Telefone  {
 
-	private static final long serialVersionUID = 1L;
+	
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	//@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	/*
 	 * @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "increment")
 	 * 
 	 * @GenericGenerator(name = "increment", strategy = "increment")
 	 */
-	 
+	@NotNull
 	private Long id;
 	
 	@NotNull (message = "O campo não deve estar vazio!")
