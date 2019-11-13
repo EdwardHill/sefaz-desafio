c95d9e9232cf842702ab7adc3c6f6d9c31a5f29b
carlos eduardo <carlo5_3duardo@hotmail.com>
2019-11-13 09:59:30 -0300

    nada

--- src/main/java/br/com/sefaz/desafio/models/Telefone.java
+++ src/main/java/br/com/sefaz/desafio/models/Telefone.java
@@ -20,7 +20,7 @@ public class Telefone implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	/*
 	 * @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "increment")
 	 * 
