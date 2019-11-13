9c41c069d6767e8f92faee7b6bf5c76261c9fea7
carlos eduardo <carlo5_3duardo@hotmail.com>
2019-11-13 09:14:01 -0300

    testede herokubd

--- src/main/java/br/com/sefaz/desafio/models/Usuario.java
+++ src/main/java/br/com/sefaz/desafio/models/Usuario.java
@@ -24,7 +24,7 @@ public class Usuario implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	//@GeneratedValue(strategy = GenerationType.IDENTITY)
+	//@GeneratedValue(strategy = GenerationType.AUTO)
 	@GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "increment")
 	@GenericGenerator(name = "increment", strategy = "increment")
 
